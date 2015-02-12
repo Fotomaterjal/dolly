6d8b7edbd61b68f5ab0ed265e1acbe8232cb43c2
Sten-Oliver Salumaa <salumaa11@gmail.com>
2015-02-12 11:42:36 +0200

    Cleaned up the main code. I hope I didn't brake anything.

--- Progemine/Program/Main/first/first/first.c
+++ Progemine/Program/Main/first/first/first.c
@@ -18,7 +18,6 @@ void init_Timer1();
 void setCurrentLimiter_T4(int ocr_value);
 int getOSCR1A(int freq);
 void init_UART();
-//void setPWMForFun();
 
 void sendString(char name[]);
 void sendLetter(uint8_t letter);
@@ -38,8 +37,8 @@ int main(void){
 	
 	cli();
 	init_Timer1();
-	//TEST WITH POWER SUPPLY
-	setCurrentLimiter_T4(32);	// 3,2% duty cycle
+	// !!!TEST WITH POWER SUPPLY!!!
+	setCurrentLimiter_T4(8);	// 3,2% duty cycle
 	sei();
 	
 	char str[] = "JOUJOU";
@@ -49,8 +48,7 @@ int main(void){
 	DDRD |= led;	
 	//init_UART();		
 
-	//setPWMForFun();
-	
+
 	
     while(1)
     {
@@ -67,7 +65,6 @@ void init_Timer1(){
 	TCCR1B = (1<<CS12 | 1<<CS10 | 1<<WGM12); // enable CTC
 	// set CTC value
 	OCR1A = OCR1A_value;	
-	
 	// enable Timer Overflow Interrupt
 	TIMSK1 |= (1<<OCIE1A);
 	// Global Interrupt enable
@@ -92,60 +89,34 @@ SIGNAL(TIMER1_COMPA_vect){
 	asm("NOP");
 }
 
-// current limit set
+// current limit set, TIMER4 PWM
 // I_tripMax = V_ref/(8*R_s), R_s = 0.1 Ohm
 // 200 mA = V_ref/0.8 -> V_ref = 0.16V = 3.2% uptime -> OCR4D = 8
 void setCurrentLimiter_T4(int ocr_value){
-	DDRD |= (1<<PD7);			//OCR4D to output
-	OCR4D = ocr_value;
-	//TCCR4A = 0x00;
-	//TCCR4B = 0x00;
-	//TCCR4C = 0x00;
-	
+	DDRD |= (1<<PD7);					//OCR4D to output
+	OCR4D = ocr_value;					// set count limit
 	TCCR4C |= (1<<COM4D1)|(1<<PWM4D);	// set fast PWM mode - clear on CM, enable PWM @ OCR4D
-	TCCR4B |= (1<<CS40);
-	//TCCR4D = 0x00;
-	TCNT4 = 0x000;	
+	TCCR4B |= (1<<CS40);				// set CPU clock for timer clock
+	TCNT4 = 0x000;						// init count
 }
-
-//void setCurrentLimiter_T4_2(){
-	//DDRD |= (1<<PD7);
-	//OCR4D = 255;
-	//
-//}
-
-//void setPWMForFun(){
-	////PC6 OC3A
-	////WGM: 8-bit fast pwm
-	//// CS: no prescale
-	//// com3b1: clear oc3a on compare match
-	//TCCR3B |= (1<<CS30) | (1<<WGM32);
-	//TCCR3A |= (1<<WGM30) | (1<<COM3A1);
-	//DDRC |= (1<<PC6);
-	//OCR3A = 32000;
-	//
-//}
-
 
 void init_UART(){
 	// UART
 	// setting baud rate to 9600
 	UBRR1 = 207;
 	// setting up double speed mode
-	UCSR1A = (1 << U2X1);
-	
+	UCSR1A = (1 << U2X1);	
 	// set up 8 data bits
 	UCSR1C |= (1<<UCSZ10) | (1<<UCSZ11);
-	// put DDR UART to output
-	//DDRD |= (1<<PD3);
 	// enable transmitter
 	UCSR1B |= (1<<TXEN1);
 	
 }
 
 void sendLetter(uint8_t letter){
+	// while last value not yet cleared
 	while ((UCSR1A & (1 << UDRE1)) == 0) ;
-	//load value to UART sending register
+	// else load value to UART sending register
 	UDR1 = letter;
 }
 
