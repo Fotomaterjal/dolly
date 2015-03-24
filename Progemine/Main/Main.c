f1cb701f9eb07c809a02a4638b748fb6dc32aa92
Sten-Oliver Salumaa <salumaa11@gmail.com>
2015-03-24 21:07:31 +0200

    Added all three stepper support and movement.

--- Progemine/Main/Main.c
+++ Progemine/Main/Main.c
@@ -16,8 +16,19 @@
 #define STEPDIV 1
 #define T1OCA_DIV 1024
 
+
 // Declare functions in this file
-void init_Timer1();
+void init_Timer1_A(uint16_t CTC_value);
+void init_Timer1_B(uint16_t CTC_value);
+void init_Timer1_C(uint16_t CTC_value);
+void changeDistDir(uint8_t dir);
+void changeHorRotDir(uint8_t dir);
+void changeVerRotDir(uint8_t dir);
+void init_Distance_Axis(uint16_t OCR1A_intake, uint8_t direction);
+void init_Timer0();
+void halt_Timer1_A();
+void halt_Timer1_B();
+void halt_Timer1_C();
 void setCurrentLimiter_T4(int ocr_value);
 int getOSCR1A(int freq);
 void init_UART();
@@ -31,14 +42,20 @@ void sendWord(uint16_t word);
 struct keyFrame readKeyframe(uint8_t bits);
 uint16_t USARTgetWord();
 uint8_t USARTgetLetter();
-uint16_t calcOCR1A(uint16_t recDistance, uint16_t recTime);
+uint8_t getDistDir(uint16_t curDist, uint16_t futureDist);
+uint8_t getHorRotDir(uint16_t curHorDeg, uint16_t futureHorDeg);
+uint16_t calcOCR1A(uint16_t curTime, uint16_t curDist, uint16_t recDistance, uint16_t recTime);
+uint16_t calcOCR1B(uint16_t curTime, uint16_t curHorDeg, uint16_t recHorDeg, uint16_t recTime);
+uint16_t calcOCR1C(uint16_t curTime, uint16_t curVerDeg, uint16_t recVerDeg, uint16_t recTime);
 
 // Define global variables
 int freq1 = 1;
 uint8_t keyFramePointer = 0;
 uint8_t keyFrameReadPointer = 0;
+uint8_t curKeyFramePointer = 0;
 uint8_t sendPointer = 0;
-int OCR1A_value;
+uint8_t startedFlag = 0;
+
 uint16_t curDist = 0;
 uint16_t curHorDeg = 0;
 uint16_t curVerDeg = 0;
@@ -47,6 +64,11 @@ uint16_t curTime = 0;
 uint8_t usartReceivePrevByte = 0;
 uint8_t usartReceiveNowByte = 1;
 
+uint8_t TimerCounter0PreSc = 0;
+
+uint16_t OCR1A_value = 15625;
+uint16_t OCR1B_value = 15625;
+uint16_t OCR1C_value = 15625;
 
 struct keyFrame{
 	uint16_t distance;
@@ -68,56 +90,124 @@ int main(void){
 	int led = 0x20;
 	DDRD |= led;
 	
-	////////////// Init Stepper CurrentLimit & DDRs //////
+	////////////// Initialize Stepper CurrentLimit & DDRs //////
 	cli();
-	init_Timer1();
+	
+	init_Timer0();
 	// !!!TEST WITH POWER SUPPLY!!!
 	setCurrentLimiter_T4(8);	// 3,2% duty cycle
 	init_steppers();			// PORT & DDR stuff for step and direction
 	sei();
 	
 	////////////// UART testing //////////////////////////
-	char str[] = "JOUJOU";
 	init_UART();		// settings and RX TX enable stuff
-
-	OCR1A = 15625;
+	
+	
+	// initialize timer
+	OCR1A = OCR1A_value;
+	// initialize 1-second CTC
+	//OCR1C = 15625;
 	///////////// Main loop //////////////////////////////
     while(1)
     {
-		for(uint32_t k = 0; k<2000000; k++){
-			asm("nop");	
-		}
-		// Debugging
-		sendWord(keyframes[sendPointer].distance);
-		sendWord(keyframes[sendPointer].horDeg);
-		sendWord(keyframes[sendPointer].vertDeg);
-		sendWord(keyframes[sendPointer].timeStamp);
 		
-		if (sendPointer < 9){
-			sendPointer++;
-		}else{
-			sendPointer = 0;
-			sendLetter(0xFF);
-			sendLetter(0xFF);
-			sendLetter(0xFF);
-			sendLetter(0xFF);
-		}
+		//if(curTime >= keyframes[curKeyFramePointer].timeStamp){
+			//curKeyFramePointer++;
+		//}
+		//if(curKeyFramePointer >= 9){
+			//stopSequence();
+		//}
+		
+		
+		
+		//TODO: problem here:
+		//somehow doesn't go into this if clause
+		//for(uint32_t k = 0; k<1000000; k++){
+			//asm("nop");
+		//}
+		//sendLetter(startedFlag);
+		//if(startedFlag == 0x01){
+			//sendLetter(0x02);
+			//sendWord(keyframes[curKeyFramePointer].distance);
+			//sendWord(keyframes[curKeyFramePointer].horDeg);
+			//sendWord(keyframes[curKeyFramePointer].vertDeg);
+			//sendWord(keyframes[curKeyFramePointer].timeStamp);
+			//
+			//
+			//
+			//sendWord(OCR1A);
+			//if(OCR1A != OCR1A_value){
+			//
+			//}
+		//}
+		//
+		
+		
+		
+		
+		
+		//for(uint32_t k = 0; k<2000000; k++){
+			//asm("nop");	
+		//}
+		//// Debugging
+		//sendWord(keyframes[sendPointer].distance);
+		//sendWord(keyframes[sendPointer].horDeg);
+		//sendWord(keyframes[sendPointer].vertDeg);
+		//sendWord(keyframes[sendPointer].timeStamp);
+		//
+		//if (sendPointer < 9){
+			//sendPointer++;
+		//}else{
+			//sendPointer = 0;
+			//sendLetter(0xFF);
+			//sendLetter(0xFF);
+			//sendLetter(0xFF);
+			//sendLetter(0xFF);
+		//}
 		
     }
 	
 }
 
 
+void startLapse(){
+	startedFlag = 0x01;
+	// init linear motion timer
+	
+	OCR1A_value = calcOCR1A(curTime, curDist, keyframes[curKeyFramePointer].distance, keyframes[curKeyFramePointer].timeStamp);
+	//OCR1A = OCR1A_value;
+	init_Timer1_A(OCR1A_value);
+}
+
+
 //calculates needed OCR1A value to travel needed distance in given time
 //linear horizontal movement
 //recDistance given in steps, recTime in seconds
-uint16_t calcOCR1A(uint16_t recDistance, uint16_t recTime){
-	float wantedStepFreq = (recDistance*STEPDIV)/recTime;
+uint16_t calcOCR1A(uint16_t curTime, uint16_t curDist, uint16_t recDistance, uint16_t recTime){
+	float wantedStepFreq = ((recDistance-curDist)*STEPDIV)/(recTime-curTime);
 	TCNT1 = 0x00;
 	uint16_t wat = (F_CPU/T1OCA_DIV)/wantedStepFreq;
-	sendLetter('u');
-	sendWord(wat);
 	return wat;	
+}
+
+//calculates needed OCR1B value to travel needed rotation in given time
+//horizontal rotational movement
+//recHorDeg given in steps, recTime in seconds
+uint16_t calcOCR1B(uint16_t curTime, uint16_t curHorDeg, uint16_t recHorDeg, uint16_t recTime){
+	float wantedStepFreq = ((recHorDeg-curHorDeg)*STEPDIV)/(recTime-curTime);
+	TCNT1 = 0x00;
+	uint16_t wat = (F_CPU/T1OCA_DIV)/wantedStepFreq;
+	return wat;
+}
+
+//calculates needed OCR1C value to travel needed rotation in given time
+//vertical rotational movement
+//recVerDeg given in steps, recTime in seconds
+uint16_t calcOCR1C(uint16_t curTime, uint16_t curVerDeg, uint16_t recVerDeg, uint16_t recTime){
+	float wantedStepFreq = ((recVerDeg-curVerDeg)*STEPDIV)/(recTime-curTime);
+	TCNT1 = 0x00;
+	uint16_t wat = (F_CPU/T1OCA_DIV)/wantedStepFreq;
+	return wat;
 }
 
 
@@ -130,22 +220,22 @@ struct keyFrame readKeyframe(uint8_t bytes){
 			case 0:
 				//sendLetter('b');
 				receivedKeyframe.distance = USARTgetWord();
-				sendWord(receivedKeyframe.distance);
+				//sendWord(receivedKeyframe.distance);
 				break;
 			case 1:
 				//sendLetter('c');
 				receivedKeyframe.horDeg = USARTgetWord();
-				sendWord(receivedKeyframe.horDeg);
+				//sendWord(receivedKeyframe.horDeg);
 				break;
 			case 2:
 				//sendLetter('d');				
 				receivedKeyframe.vertDeg = USARTgetWord();
-				sendWord(receivedKeyframe.vertDeg);
+				//sendWord(receivedKeyframe.vertDeg);
 				break;
 			case 3:
 				//sendLetter('e');				
 				receivedKeyframe.timeStamp = USARTgetWord();				
-				sendWord(receivedKeyframe.timeStamp);
+				//sendWord(receivedKeyframe.timeStamp);
 				break;
 		}
 	}
@@ -178,22 +268,132 @@ uint8_t USARTgetLetter(){
 }
 
 //Initialize Timer1
-void init_Timer1(){	
+void init_Timer1_A(uint16_t CTC_value){	
+	TCNT1 = 0x0000;
 	TCCR1A = 0x00;
 	// setting CPU clock/1024
 	// 1s = 15625 cycles
 	TCCR1B = (1<<CS12 | 1<<CS10 | 1<<WGM12); // enable CTC
 	// set CTC value
-	OCR1A = OCR1A_value;	
-	// enable Timer Overflow Interrupt
+	OCR1A = CTC_value;	
+	// enable Timer Overflow Interrupt for COMPA
 	TIMSK1 |= (1<<OCIE1A);
 	// Global Interrupt enable
 	SREG |= (1<<7);	
 }
 
+void init_Timer1_B(uint16_t CTC_value){
+	TCNT1 = 0x0000;
+	TCCR1A = 0x00;
+	// setting CPU clock/1024
+	// 1s = 15625 cycles
+	TCCR1B = (1<<CS12 | 1<<CS10 | 1<<WGM12); // enable CTC
+	// set CTC value
+	OCR1B = CTC_value;
+	// enable Timer Overflow Interrupt for COMPA
+	TIMSK1 |= (1<<OCIE1B);
+	// Global Interrupt enable
+	SREG |= (1<<7);
+}
+
+void init_Timer1_C(uint16_t CTC_value){
+	TCNT1 = 0x0000;
+	TCCR1A = 0x00;
+	// setting CPU clock/1024
+	// 1s = 15625 cycles
+	TCCR1B = (1<<CS12 | 1<<CS10 | 1<<WGM12); // enable CTC
+	// set CTC value
+	OCR1C = CTC_value;
+	// enable Timer Overflow Interrupt for COMPA
+	TIMSK1 |= (1<<OCIE1C);
+	// Global Interrupt enable
+	SREG |= (1<<7);
+}
+
+void halt_Timer1_A(){
+	//TCNT1 = 0x0000;
+	// disable Timer Overflow Interrupt for COMPA
+	TIMSK1 |= (0<<OCIE1A);
+}
+
+void halt_Timer1_B(){
+	//TCNT1 = 0x0000;
+	// disable Timer Overflow Interrupt for COMPB
+	TIMSK1 |= (0<<OCIE1B);
+}
+
+void halt_Timer1_C(){
+	//TCNT1 = 0x0000;
+	// disable Timer Overflow Interrupt for COMPA
+	TIMSK1 |= (0<<OCIE1C);
+}
+
+void init_Timer0(){
+	//set mode CTC
+	//TCCR0A = (1<<WGM01);
+	//set preScaler to 1024
+	TCCR0B = (1<<CS02 | 1<<CS00);
+	//allow OVF interrupt
+	TIMSK0 = (1<<TOIE0);	
+}
+
 // get OSCR1A value for required frequency
 int getOSCR1A(int freq){
 	return (F_CPU/CKDIV_16/freq);	
+}
+
+//returns future direction
+uint8_t getDistDir(uint16_t curDist, uint16_t futureDist){
+	if(futureDist < curDist){
+		return 0x00;	//moving backwards
+	}else{
+		return 0x01;	//moving forward
+	}
+}
+
+//returns future direction
+uint8_t getHorRotDir(uint16_t curHorDeg, uint16_t futureHorDeg){
+	if(futureHorDeg < curHorDeg){
+		return 0x00;	//moving backwards
+	}else{
+		return 0x01;	//moving forward
+	}
+}
+
+
+void changeDistDir(uint8_t dir){
+	if(dir){
+		// Change upper stepper DIR to FORWARD movement
+		DDRD |= (1 << PD1);
+	}else{
+		// Change upper stepper DIR to BACKWARD movement
+		DDRD &= ~(1 << PD1);
+	}	
+}
+
+void changeHorRotDir(uint8_t dir){
+	if(dir){
+		// Change middle stepper DIR to FORWARD movement
+		DDRC |= (1 << PD7);
+		}else{
+		// Change middle stepper DIR to BACKWARD movement
+		DDRC &= ~(1 << PD7);
+	}
+}
+
+void changeVerRotDir(uint8_t dir){
+	if(dir){
+		// Change upper stepper DIR to FORWARD movement
+		DDRB |= (1 << PB4);
+		}else{
+		// Change upper stepper DIR to BACKWARD movement
+		DDRB &= ~(1 << PB4);
+	}
+}
+
+void init_Distance_Axis(uint16_t OCR1A_intake, uint8_t direction){
+	changeDistDir(direction);
+	init_Timer1_A(OCR1A_intake);
 }
 
 
@@ -307,6 +507,8 @@ SIGNAL(TIMER1_COMPA_vect){
 	//TEST WITHH POWER SUPPLY
 	//PB6 = STEP1
 	//PORTD ^= (1 << PD1);
+	curDist ++;
+	//upper stepper
 	PORTB = PORTB^(1<<PB6);
 	asm("NOP");
 	asm("NOP");
@@ -320,16 +522,85 @@ SIGNAL(TIMER1_COMPA_vect){
 //Interrupt, got data from HC-06 module
 SIGNAL(USART1_RX_vect){
 	uint8_t firstByte = USARTgetLetter();
+	//sendLetter(firstByte);
 	uint8_t secondByte;
 	
-	if (firstByte == 'k'){	//gonna be receiving a keyframe, aka four 16bit numbers
-		
-		keyframes[keyFrameReadPointer] = readKeyframe(2);
-	
+	if (firstByte == 'k'){	//gonna be receiving a keyFrame, aka four 16bit numbers		
+		keyframes[keyFrameReadPointer] = readKeyframe(2);	
 	}else if(firstByte == 's'){	//receiving some metadata (exactly which keyFrame is coming)
 		secondByte = USARTgetLetter();
 		keyFrameReadPointer = secondByte;
 	}else if(firstByte == 'z'){
 		startLapse();
 	}
+}
+
+SIGNAL(TIMER0_OVF_vect){			
+	if(TimerCounter0PreSc != 61){
+		TimerCounter0PreSc++;
+	}else{							// happens every second
+		//PORTD = PORTD^(1<<PD5);	// invert LED value	
+		TimerCounter0PreSc = 0;
+		//sendLetter(startedFlag);
+		if(startedFlag == 0x01){
+			curTime++;
+			sendLetter(0x02);
+			sendWord(keyframes[curKeyFramePointer].distance);
+			sendWord(keyframes[curKeyFramePointer].horDeg);
+			sendWord(keyframes[curKeyFramePointer].vertDeg);
+			sendWord(keyframes[curKeyFramePointer].timeStamp);
+			sendLetter(0xFF);
+			sendWord(OCR1A);
+			// time to change to next keyFrame
+			if(curTime >= keyframes[curKeyFramePointer].timeStamp){
+				curKeyFramePointer++;	//go to next keyframe
+				// OCR1A = linear movement
+				OCR1A_value = calcOCR1A(curTime, curDist, keyframes[curKeyFramePointer].distance, keyframes[curKeyFramePointer].timeStamp);
+				// OCR1B = rotational movement
+				OCR1B_value = calcOCR1B(curTime, curHorDeg, keyframes[curKeyFramePointer].horDeg, keyframes[curKeyFramePointer].timeStamp);
+				// OCR1C = vertical rotational movemnt
+				OCR1B_value = calcOCR1C(curTime, curVerDeg, keyframes[curKeyFramePointer].vertDeg, keyframes[curKeyFramePointer].timeStamp);
+				
+				////////////////////////////////////////////////// LINEAR MOTION STUFF /////////////////////////////////////////////////
+				if(OCR1A_value == 0xFFFF){		// divide by 0 aka stepper has to stop
+					halt_Timer1_A();
+				}else{
+					if(getDistDir(curDist, keyframes[curKeyFramePointer].distance) != 0x01){
+						//we have to move backwards
+						changeDistDir(0);		//now we are moving backwards
+					}else{
+						changeDistDir(1);		//move forward
+					}
+					init_Timer1_A(OCR1A_value);	//initialize timer with new OCR1A value
+				}
+				
+				///////////////////////////////////////////////// HORIZONTAL ROTATE STUFF //////////////////////////////////////////////
+				if(OCR1B_value == 0xFFFF){		// divide by 0 aka stepper has to stop
+					halt_Timer1_B();
+				}else{
+					if(getHorRotDir(curHorDeg, keyframes[curKeyFramePointer].horDeg) != 0x01){
+						//we have to move backwards
+						changeHorRotDir(0);		//now we are moving backwards
+					}else{
+						changeHorRotDir(1);		//move forward
+					}
+					init_Timer1_B(OCR1B_value);	//initialize timer with new OCR1A value
+				}
+				///////////////////////////////////////////////// VERTICAL ROTATE STUFF ////////////////////////////////////////////////
+				if(OCR1B_value == 0xFFFF){		// divide by 0 aka stepper has to stop
+					halt_Timer1_C();
+				}else{
+					if(getHorRotDir(curVerDeg, keyframes[curKeyFramePointer].vertDeg) != 0x01){
+						//we have to move backwards
+						changeVerRotDir(0);		//now we are moving backwards
+					}else{
+						changeVerRotDir(1);		//move forward
+					}
+					init_Timer1_C(OCR1C_value);	//initialize timer with new OCR1A value
+				}
+				////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////							
+			}						
+		}		
+	}	
+	PORTD = PORTD^(1<<PD5);	// invert LED value	every second
 }
